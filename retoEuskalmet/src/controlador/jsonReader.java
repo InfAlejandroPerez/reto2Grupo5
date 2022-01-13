7709a0a2bfb4f039aab00752abdaf830cd13b237
in2dam-b <in2dam-b@elorrieta.int>
2022-01-13 20:33:23 +0100

    lectura de Json e impresion por consola

---
+++ retoEuskalmet/src/controlador/jsonReader.java
@@ -0,0 +1,93 @@
+package controlador;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+
+import modelo.Municipios;
+import modelo.Provincias;
+
+public class jsonReader {
+	public static List<Municipios> municipios = new ArrayList<Municipios>();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JsonParser parser = new JsonParser();
+		final String url = "pueblos.json";
+
+		try {
+
+			FileReader fr = new FileReader(url);
+			JsonElement datos = parser.parse(fr);
+
+			JsonArray array = datos.getAsJsonArray();
+			Iterator<JsonElement> iter = array.iterator();
+			while (iter.hasNext()) {
+				Municipios municipio = new Municipios();
+				Provincias provincia = new Provincias();
+				JsonElement entrada = iter.next();
+				JsonObject objeto = entrada.getAsJsonObject();
+				Iterator<Map.Entry<String, JsonElement>> iter2 = objeto.entrySet().iterator();
+				
+				for(Map.Entry<String, JsonElement> entry : objeto.entrySet()) {
+					
+					if (entry.getKey().equals("documentName")) {
+						municipio.setNombre(entry.getValue().getAsString());
+					}
+					if (entry.getKey().equals("municipalitycode")) {
+						
+						if(entry.getValue().getAsString().contains(" ")){
+					        String formatCod = entry.getValue().getAsString().substring(0, entry.getValue().getAsString().indexOf(" ")); 
+					        municipio.setCodMunicipio(Integer.parseInt(formatCod));
+					     }else {
+					    	 municipio.setCodMunicipio(entry.getValue().getAsInt());
+					     }
+					}
+					if (entry.getKey().equals("turismDescription")) {
+						municipio.setDescripcion(entry.getValue().getAsString());
+					}
+					if (entry.getKey().equals("territory")) {
+						provincia.setNomProvincia(entry.getValue().getAsString());
+					}
+					if (entry.getKey().equals("territorycode")) {
+						if(entry.getValue().getAsString().contains(" ")){
+					        String formatCod2 = entry.getValue().getAsString().substring(0, entry.getValue().getAsString().indexOf(" ")); 
+					        provincia.setCodProvincia(Integer.parseInt(formatCod2));
+					     }else {
+					    	 provincia.setCodProvincia(entry.getValue().getAsInt());
+					     }
+					}
+					municipio.setProvincias(provincia);
+				}
+				municipios.add(municipio);
+
+				
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		
+		
+		System.out.println(municipios.size());
+		for (int i = 0; i < municipios.size(); i++) {
+            System.out.println("Nombre Municipio: "+ municipios.get(i).getNombre());
+            System.out.println("Cod. Municipio: "+ municipios.get(i).getCodMunicipio());
+            System.out.println("Descripcion: "+ municipios.get(i).getDescripcion());
+            System.out.println("Provincia: "+ municipios.get(i).getProvincias().getNomProvincia());
+            System.out.println("--------------------------------");
+        }
+		//System.out.println(municipios);
+	}
+}
